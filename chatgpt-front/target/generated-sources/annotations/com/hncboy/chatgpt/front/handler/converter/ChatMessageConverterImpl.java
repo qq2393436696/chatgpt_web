54d1a29ddf806d2c15d342ff09f84c034b199539
root <2393436696@qq.com>
2023-06-28 17:55:04 +0800

    init

---
+++ chatgpt-front/target/generated-sources/annotations/com/hncboy/chatgpt/front/handler/converter/ChatMessageConverterImpl.java
@@ -0,0 +1,11 @@
+package com.hncboy.chatgpt.front.handler.converter;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-28T17:45:08+0800",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.6 (Red Hat, Inc.)"
+)
+public class ChatMessageConverterImpl implements ChatMessageConverter {
+}
